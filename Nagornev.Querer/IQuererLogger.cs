60fd65a9e9114fc59dbcfafbad741d45129fea48
nagornev <pavel.nagornev@ya.ru>
2024-09-20 00:08:41 +0300

    Added logger and base querer exception.

---
+++ Nagornev.Querer/IQuererLogger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Nagornev.Querer
+{
+    public interface IQuererLogger
+    {
+        void Inform(string message);
+
+        void Warn(string message);
+
+        void Error<TExceptionType>(TExceptionType exception, Func<TExceptionType, string> message)
+            where TExceptionType : Exception;
+    }
+}
---
+++ Nagornev.Querer/QuererException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Nagornev.Querer
+{
+    public class QuererException : Exception
+    {
+        public QuererException()
+        {
+        }
+
+        public QuererException(string message) 
+            : base(message)
+        {
+        }
+
+        public QuererException(string message,
+                               Exception innerException) 
+            : base(message, innerException)
+        {
+        }
+    }
+}
